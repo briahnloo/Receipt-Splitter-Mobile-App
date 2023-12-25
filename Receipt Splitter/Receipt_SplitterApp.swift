7b822ea63f02aad7162d4dba6f5194aee6cd4fe7
briahnloo <brian.liu2021@gmail.com>
2023-12-25 14:59:58 -0800

    Initial Commit

---
+++ Receipt Splitter/Receipt_SplitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Receipt_SplitterApp.swift
+//  Receipt Splitter
+//
+//  Created by Brian Liu on 12/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Receipt_SplitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
